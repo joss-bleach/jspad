4e14884e25c82710f76aacd4a59b34597729c5a2
Joss Bleach <jossbleach@josss-mbp.home>
2022-09-13 10:33:40 +0100

    Refactored unpkg fetch plugin.

--- src/plugins/unpkg-fetch-plugin.ts
+++ src/plugins/unpkg-fetch-plugin.ts
@@ -10,41 +10,50 @@ export const unpkgFetchPlugin = (inputCode: string) => {
   return {
     name: "unpkg-fetch-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/, namespace: "a" }, async (args: any) => {
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputCode,
-          };
-        }
+      build.onLoad({ filter: /^index.js/ }, () => {
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+      });
 
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const cachedResult =
           await packageBundleCache.getItem<esbuild.OnLoadResult>(args.path);
         if (cachedResult) {
           return cachedResult;
         }
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
-
-        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
 
         const escapedCSS = data
           .replace(/\n/g, "")
           .replace(/"/g, '\\"')
           .replace(/'/g, "\\'");
 
-        const contents =
-          fileType === "css"
-            ? `
+        const contents = `
           const style = document.createElement('style');
           style.innerText = '${escapedCSS}';
-          document.head.appendChild(style);
-        `
-            : data;
+          document.head.appendChild(style);`;
 
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
-          contents: contents,
+          contents,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
+        await packageBundleCache.setItem(args.path, result);
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/, namespace: "a" }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
 
